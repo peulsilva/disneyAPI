cba9e59fe9b258496be6c094d503d62bea4e940b
matrodrigues123 <matheusraofc@outlook.com>
2021-11-21 10:15:56 -0300

    add swagger documentation

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const setRoutes = require('./setRoutes')
-const app = express()
-const port = 1500
+const express = require("express");
+const setRoutes = require("./setRoutes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger.json");
+const app = express();
+const port = 1500;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-const main = app=>{
-    setRoutes(app);
-}
+const main = (app) => {
+  setRoutes(app);
+};
 
-main(app)
+main(app);
